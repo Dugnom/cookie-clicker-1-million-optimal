575398b460b43567157f86b054f201ea9da12395
OldMcCrusher <stevenleonhardt@gmx.de>
2019-09-03 21:50:55 +0200

    searching a list entry

--- MonteCarlo.py
+++ MonteCarlo.py
@@ -1,4 +1,5 @@
-import random
+import random 
+import matplotlib.pyplot as plt
 
 
 def picknumber(x, y):
@@ -7,14 +8,31 @@ def picknumber(x, y):
 
 buildings = [1, 0, 0, 0, 0]  # Cursor, Grandma, Farm, Mine, Factory
 # Cursor, Grandma, Farm, Mine, Factory
-upgrades = [[0, 0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0], []]
+upgrades = [[0, 0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0]]
 
 
-def propose_purchase(buildings, ugrades):
+def propose_purchase(buildings, upgrades):
     randomnumber = picknumber(0, 16)
     if randomnumber <5:
         buildings[randomnumber]=buildings[randomnumber]+1
-    return buildings
+ 
+    else:
+        if randomnumber <9:
+            upgrades[0][randomnumber-5]=upgrades[0][randomnumber-5]+1
+        elif randomnumber <12:
+            upgrades[1][randomnumber-9]=upgrades[1][randomnumber-9]+1
+        elif randomnumber <15:
+            upgrades[2][randomnumber-12]=upgrades[2][randomnumber-12]+1
+        elif randomnumber <17:
+            upgrades[3][randomnumber-15]=upgrades[3][randomnumber-15]+1
+        else:
+            print("error")
+
+    return buildings, upgrades
+
+
+
+
 
 
 print(propose_purchase(buildings, upgrades))
