e2fba81b4c0d0602d84ecc685bd5d90ba3b00867
OldMcCrusher <stevenleonhardt@gmx.de>
2019-09-04 18:26:25 +0200

    Listenauswahl angepasst, fehler immernoch da

--- MonteCarlo.py
+++ MonteCarlo.py
@@ -1,6 +1,6 @@
 import random
 import matplotlib.pyplot as plt
-
+import copy
 
 def picknumber(x, y):
     return random.randint(x, y)
@@ -8,41 +8,44 @@ def picknumber(x, y):
 
 
 
-def propose_purchase(buildings_0, upgrades_0):
+def propose_purchase(buildings_1, upgrades_1):
     randomnumber = picknumber(0, 16)
+    buildings_0 = copy.copy(buildings_1)
+    upgrades_0 = copy.copy(upgrades_1)
     if randomnumber < 5:
-        buildings_0[randomnumber] = buildings_0[randomnumber]+1
+        buildings_1[randomnumber] = buildings_1[randomnumber]+1
     elif randomnumber < 9:
-        upgrades_0[0][randomnumber-5] = upgrades_0[0][randomnumber-5]+1
+        upgrades_1[0][randomnumber-5] = upgrades_1[0][randomnumber-5]+1
     elif randomnumber < 12:
-        upgrades_0[1][randomnumber-9] = upgrades_0[1][randomnumber-9]+1
+        upgrades_1[1][randomnumber-9] = upgrades_1[1][randomnumber-9]+1
     elif randomnumber < 15:
-        upgrades_0[2][randomnumber-12] = upgrades_0[2][randomnumber-12]+1
+        upgrades_1[2][randomnumber-12] = upgrades_1[2][randomnumber-12]+1
     elif randomnumber < 17:
-        upgrades_0[3][randomnumber-15] = upgrades_0[3][randomnumber-15]+1
+        upgrades_1[3][randomnumber-15] = upgrades_1[3][randomnumber-15]+1
     else:
         print("error")
 
-    return buildings_0, upgrades_0, randomnumber
+    return buildings_1, upgrades_1, buildings_0, upgrades_0, randomnumber
 
 
-def check_possibility(buildings_0, upgrades_0):
-    notFound = True
-    while notFound:
-        buildings_1, upgrades_1, randomnumber = propose_purchase(buildings_0.copy(), upgrades_0.copy())
+def check_possibility(buildings_1, upgrades_1):
+        buildings_1, upgrades_1, buildings_0, upgrades_0, randomnumber = propose_purchase(buildings_1, upgrades_1)
+        print (buildings_0, upgrades_0, "buildings_0")
         if randomnumber<5:
-            notFound =  False
-        elif randomnumber<9:
-            notFound =  False
-        elif randomnumber<12 and buildings_0[1]>0:
-            notFound =  False
-        elif randomnumber<15 and buildings_0[2]>0:
-            notFound =  False
-        elif randomnumber<17 and buildings_0[3]>0:
-            notFound =  False
+            return buildings_1, upgrades_1
+        elif randomnumber<9 and buildings_1[0]>0:
+            return buildings_1, upgrades_1
+        elif randomnumber<12 and buildings_1[1]>0:
+            return buildings_1, upgrades_1
+        elif randomnumber<15 and buildings_1[2]>0:
+            return buildings_1, upgrades_1
+        elif randomnumber<17 and buildings_1[3]>0:
+            return buildings_1, upgrades_1
         else:
-            notFound =  True
-    return buildings_1, upgrades_1
+            buildings_1 = copy.copy(buildings_0)
+            upgrades_1 = copy.copy(upgrades_0)
+            return buildings_1, upgrades_1
+
 
 buildings = [1, 0, 0, 0, 0]  # Cursor, Grandma, Farm, Mine, Factory
 # Cursor, Grandma, Farm, Mine, Factory
