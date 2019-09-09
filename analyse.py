9165c5488e2620c95bee36971f706691531804a5
David Schwer <davidmanni@gmx.de>
2019-09-09 22:41:24 +0200

    change to steven version

--- analyse.py
+++ analyse.py
@@ -3,6 +3,6 @@ import matplotlib.pyplot as plt
 list2 = []
 with open("ccSolutions") as f:
     for row in f:
-        list2.append(float(row.split()[0]))
+        list2.append(row.split(";")[0])
 
 print(min(list2))
